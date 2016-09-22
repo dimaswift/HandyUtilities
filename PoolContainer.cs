0478a034c1513db8a6e379e7dd1d41a57f1ea034
dimaswift <dimaswift@gmail.com>
2016-09-22 08:12:56 +0300

    fixed compile time error

--- PoolContainer.cs
+++ PoolContainer.cs
@@ -6,9 +6,11 @@ namespace HandyUtilities.PoolSystem
     {
         public static SO CreateScriptableObjectAsset<SO>(string name) where SO : ScriptableObject
         {
+#if UNITY_EDITOR
             var path = UnityEditor.EditorUtility.SaveFilePanelInProject("Save Instance", name, "asset", "Choose Folder", Application.dataPath);
             UnityEditor.AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<SO>(), path);
             return UnityEditor.AssetDatabase.LoadAssetAtPath<SO>(path);
+#endif
         }
 
     }
