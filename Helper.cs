58855121638e43d275d9c172bc4f12b2a478c48b
dimaswift <dimaswift@gmail.com>
2017-10-20 14:20:18 +0300

    added trajectory method and throw velocity method

--- Helper.cs
+++ Helper.cs
@@ -35,6 +35,7 @@ namespace HandyUtilities
         }
     }
 
+
     public class Rigidbody2DPositionSaver : PositionSaver
     {
         Rigidbody2D m_body;
@@ -462,6 +463,37 @@ namespace HandyUtilities
 
         #region Static Methods 
 
+        public static Vector3 CalculateBestThrowVelocity(Vector3 origin, Vector3 target, float timeToTarget)
+        {
+            // calculate vectors
+            Vector3 toTarget = target - origin;
+            Vector3 toTargetXZ = toTarget;
+            toTargetXZ.y = 0;
+
+            // calculate xz and y
+            float y = toTarget.y;
+            float xz = toTargetXZ.magnitude;
+
+            // calculate starting speeds for xz and y. Physics forumulase deltaX = v0 * t + 1/2 * a * t * t
+            // where a is "-gravity" but only on the y plane, and a is 0 in xz plane.
+            // so xz = v0xz * t => v0xz = xz / t
+            // and y = v0y * t - 1/2 * gravity * t * t => v0y * t = y + 1/2 * gravity * t * t => v0y = y / t + 1/2 * gravity * t
+            float t = timeToTarget;
+            float v0y = y / t + 0.5f * Physics.gravity.magnitude * t;
+            float v0xz = xz / t;
+
+            // create result vector for calculated starting speeds
+            Vector3 result = toTargetXZ.normalized;        // get direction of xz but with magnitude 1
+            result *= v0xz;                                // set magnitude of xz to v0xz (starting speed in xz plane)
+            result.y = v0y;                                // set y to v0y (starting speed of y plane)
+
+            return result;
+        }
+
+        public static Vector3 PlotTrajectoryAtTime(Vector3 start, Vector3 startVelocity, float time)
+        {
+            return start + startVelocity * time + Physics.gravity * time * time * 0.5f;
+        }
 
         public static byte[] GetBytes(string base64)
         {
